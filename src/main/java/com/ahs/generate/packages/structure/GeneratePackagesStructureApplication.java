42a86a188f8346c4883cf8c710d86700d41fed85
Ana Hinojosa <ahinojosa@vectoritcgroup.com>
2018-11-22 14:51:04 +0100

    Initial commit

---
+++ src/main/java/com/ahs/generate/packages/structure/GeneratePackagesStructureApplication.java
@@ -0,0 +1,12 @@
+package com.ahs.generate.packages.structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratePackagesStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratePackagesStructureApplication.class, args);
+	}
+}
